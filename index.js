6a7dd5d470b5995e5d5a979f61774c0db3fda5d1
shahroz787 <shahrozahmed787@gmail.com>
2025-06-07 01:00:28 +0500

    index.js edit

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 import app from "./server.js";
-
-const port = process.env.PORT || 5001;
-
-app.listen(port, () => {
-  console.log(`Server is running locally on port ${port}`);
-});
+export default app;
